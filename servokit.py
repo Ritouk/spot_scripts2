257647a3bd673d3fdf0cf1075514b23ac192cd82
Ritouk <raphbenhams@gmail.com>
2024-05-18 19:33:53 +0300

    Added Selection of legs

--- servokit.py
+++ servokit.py
@@ -3,15 +3,33 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 print("Initializing ServoKit")
 i=0
+
+def all_movement(a):
+    #left front leg
+    kit.servo[1].angle = a
+    kit.servo[3].angle = a
+    kit.servo[5].angle = a
+    time.sleep(2)
+    #right front leg
+    kit.servo[2].angle = a
+    kit.servo[4].angle = a
+    kit.servo[6].angle = a
+    time.sleep(2)
+    #left back leg
+    kit.servo[15].angle = a
+    kit.servo[13].angle = a
+    kit.servo[11].angle = a
+    time.sleep(2)
+    #right back leg
+    kit.servo[14].angle = a
+    kit.servo[12].angle = a
+    kit.servo[10].angle = a
+    time.sleep(2)
+
+
 while(i<100):
-    #kit.servo[0].angle = 90
-    #Kit.servo[1].angle = 90
-    #kit.servo[4].angle = 90
-    kit.servo[5].angle = 60
-    time.sleep(3)
-    #kit.servo[0].angle = 120
-    #kit.servo[1].angle = 110
-    #kit.servo[4].angle = 140
-    kit.servo[5].angle = 140
-    time.sleep(3)
+    all_movement(80)
+    #time.sleep(5)
+    all_movement(120)
     i+=1
+
